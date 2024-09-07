c8a7027c9b1588831681be75a03ee82bab5e27b3
NNNACHID <n.n.nachid@protonmail.com>
2024-09-07 23:27:00 +0200

    user entity, landpage, navbar in progress

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    pass
+
+class Creator(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+
+class Advertiser(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+
+class Association(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
